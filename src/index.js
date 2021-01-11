c47e13ebcf2715fb4e129b8c9188a31033d4e4db
ghar <mohamed.gharsallah.tn@gmail.com>
2021-01-11 19:41:21 +0100

    improve responsiveness

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
-import { Anchor, grommet as defaultGrommet, Grommet, Header, Heading, Box, Main, Avatar, Paragraph, ResponsiveContext, Text } from 'grommet'
-import { deepMerge } from 'grommet/utils'
+import { Anchor, grommet, Grommet, Header, Heading, Box, Main, Avatar, Paragraph, ResponsiveContext, Text } from 'grommet'
 
 const profilePic = require('./img/mo.jpeg')
 const githubIcon = require('./img/github.png')
@@ -9,22 +8,6 @@ const instagramIcon = require('./img/instagram.png')
 const linkedInIcon = require('./img/linkedin.png')
 const twitterIcon = require('./img/twitter.png')
 const resume = require('./CV_Mohamed_Gharsallah.pdf')
-
-const grommet = deepMerge(defaultGrommet, {
-  global: {
-    breakpoints: {
-      small: {
-        value: 900,
-      },
-      medium: {
-        value: 1600,
-      },
-      large: {
-        value: 2000,
-      },
-    },
-  },
-})
 
 const WordShuffle = ({ words }) => {
   const duration = 3000
@@ -48,14 +31,13 @@ const WordShuffle = ({ words }) => {
 }
 
 const App = () => {
-  const [knowMore, setKnowMore] = useState(false)
   return (
     <Grommet theme={grommet}>
       <ResponsiveContext.Consumer>
         {(size) => (
           <>
-            <Header size="medium" pad="medium" className="header">
-              <Heading level="4">Mohamed Gharsallah</Heading>
+            <Header size="medium" pad="medium" className="header" justify={size === 'small' ? 'center' : undefined}>
+              {size !== 'small' && <Heading level="4">Mohamed Gharsallah</Heading>}
               <Box direction="row" textAlign="right" gap="medium">
                 <Anchor href="#profile" color="dark-1">
                   Profile
@@ -74,8 +56,8 @@ const App = () => {
             <Main pad={{ horizontal: 'xlarge' }} overflow="none">
               <div id="profile"></div>
               <Box justify="center" pad={{ top: 'xlarge', bottom: 'large' }} className="page-height">
-                <Box animation="slideDown" align="start" justify="center" direction={size !== 'small' ? 'row' : 'column'} gap="large" wrap>
-                  <Box width="218px" gap="medium" align="center">
+                <Box animation="slideDown" align="start" justify="center" direction={size === 'small' ? 'column' : 'row'} gap="large">
+                  <Box width="218px" gap="medium" align="center" pad={{ bottom: 'medium' }}>
                     <Avatar key="profile" size="328px" src={profilePic} round="medium" />
                     <Box direction="row" gap="medium">
                       <a target="_blank" href="https://www.linkedin.com/in/gharsallah-m/">
@@ -92,7 +74,7 @@ const App = () => {
                       </a>
                     </Box>
                   </Box>
-                  <Box width={{ max: '500px', min: '350px' }} gap="small">
+                  <Box width={{ max: '500px' }} gap="small">
                     <Heading color="dark-3" level="3">
                       Hey there 👋🏻{' '}
                     </Heading>
@@ -122,7 +104,7 @@ const App = () => {
               </Box>
               <Box id="hire" animation="slideDown" pad="large" justify="center" className="page-height">
                 <Heading style={{ maxWidth: '100%' }} color="dark-3" level="2">
-                  <WordShuffle words={['E-commerce solution', 'Api', 'Mobile app', 'Business platform']} />{' '}
+                  <WordShuffle words={['E-commerce solution', 'Api', 'Mobile app', 'Business platform']} />
                 </Heading>
                 <Heading level="1">Can do! How can I be of help?</Heading>
                 <Box pad={{ top: 'medium' }}>
